1ed7837971dca0affb5ba6297e4edd69f8c129d1
KennyVera <kennyvera43@gmail.com>
2026-01-19 18:19:05 -0500

    cambiospruebadeIntellij

---
+++ frontend/src/app/components/menuprincipal/menuprincipal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menuprincipal',
+  imports: [],
+  templateUrl: './menuprincipal.html',
+  styleUrl: './menuprincipal.css',
+})
+export class Menuprincipal {
+
+}
