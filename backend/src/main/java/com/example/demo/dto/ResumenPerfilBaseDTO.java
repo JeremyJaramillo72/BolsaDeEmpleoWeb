b2db456aa336e2762961cd55a7fd5fc35361d975
Kinetro <lquinteror@uteq.edu.ec>
2026-03-08 04:19:59 -0500

    resumenDTO

--- backend/src/main/java/com/example/demo/dto/ResumenPerfilBaseDTO.java
+++ backend/src/main/java/com/example/demo/dto/ResumenPerfilBaseDTO.java
@@ -4,18 +4,15 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class ResumenPostulacionDTO {
+public class ResumenPerfilBaseDTO {
     private String nombre;
     private String apellido;
     private String correo;
     private String urlFotoPerfil;
     private String archivoCv;
-    private Date fechaPostulacion;
+    private Date   fechaPostulacion;
     private String estadoPostulacion;
     private String mensajeEvaluacion;
-    private String formacionAcademica;
-    private String experienciaLaboral;
-    private String cursosRealizados;
-    private String idiomas;
     private String nombreEmpresa;
 }
+
