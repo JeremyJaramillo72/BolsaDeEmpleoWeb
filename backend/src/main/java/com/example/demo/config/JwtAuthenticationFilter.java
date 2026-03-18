5eda821c6f25075f33f1d44917873aa856304fd6
JeremyJaramillo72 <ljaramilloa@uteq.edu.ec>
2026-03-18 07:59:38 -0500

    quitar el @Autowired

--- backend/src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,16 +19,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtService jwtService;
 
-    @Autowired
-    private SesionRepository sesionRepository;
-
-    @Autowired
-    private UserDetailsService userDetailsService; // Necesario para la autenticación normal
+    private final JwtService jwtService;
+    private final SesionRepository sesionRepository;
+    private final UserDetailsService userDetailsService; // Necesario para la autenticación normal
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
